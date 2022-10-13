7eac4eaab80fc22431ad011f9a5143957a71deac
Michael Sprague <mikesprague@gmail.com>
2022-10-13 18:03:27 -0400

    :wrench: Tweak conf

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   publicDir: '../public',
   base: './',
   outDir: './',
-  appType: 'custom',
+  appType: 'spa',
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
